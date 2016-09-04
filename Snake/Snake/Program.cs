ce81ac3675309904f7259389edabd8cdf70c1822
Amanor96 <kravchenko_misha_1996@mail.ru>
2016-09-04 21:29:55 +0300

    Добавлен список

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -18,7 +18,34 @@ namespace Snake
 
             Console.ReadKey();
 
+
+
+            List<char> sym = new List<char>();
+            sym.Add( '@' );
+            sym.Add('#');
+            sym.Add('%');
+            sym.Add('*');
+
+            foreach(char n in sym)
+            {
+                Console.Write(n);
+            }
+            Console.ReadKey();
+
+
+            List<Point> POINT = new List<Point>();
+            POINT.Add( p1 );
+            POINT.Add( p2 );
+
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write(POINT);
+            Console.ReadKey();
         }
+
+        
+        
+
 
     }
 }
